b17d598f9fc8dc2794cd96eb4cce9c1a08f65c45
Andrea Burattin <andrea.burattin@gmail.com>
2018-05-29 19:55:23 +0200

    Fixed problem with wrong equals

--- cognitiveprom/src/main/java/cognitiveprom/projections/AggregationValues.java
+++ cognitiveprom/src/main/java/cognitiveprom/projections/AggregationValues.java
@@ -1,11 +1,20 @@
 package cognitiveprom.projections;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.deckfour.xes.model.XAttribute;
+import org.deckfour.xes.model.XAttributeContinuous;
+import org.deckfour.xes.model.XAttributeDiscrete;
+import org.deckfour.xes.model.XEvent;
 
 public class AggregationValues implements Comparable<AggregationValues> {
 
-	public static AggregationValues FREQUENCY = new AggregationValues(null, "Event frequency");
+	public static AggregationValues FREQUENCY = new AggregationValues(null, "Event frequency") {
+		@Override
+		public Number getValue(XEvent event) {
+			return 1;
+		}
+	};
+
 
 	private String specialFunction = null;
 	private String attributeName = null;
@@ -23,16 +32,28 @@ public class AggregationValues implements Comparable<AggregationValues> {
 		return attributeName;
 	}
 	
+	public Number getValue(XEvent event) {
+		XAttribute a = event.getAttributes().get(attributeName);
+		if (a instanceof XAttributeDiscrete) {
+			return ((XAttributeDiscrete) a).getValue();
+		} else if (a instanceof XAttributeContinuous) {
+			return ((XAttributeContinuous) a).getValue();
+		}
+		return 0;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
-		if (obj.getClass() != getClass()) {
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof AggregationValues)) {
 			return false;
 		}
 		AggregationValues rhs = (AggregationValues) obj;
-		return new EqualsBuilder()
-				.append(specialFunction, rhs.specialFunction)
-				.append(attributeName, rhs.specialFunction)
-				.isEquals();
+		boolean att1 = (specialFunction == null)? (rhs.specialFunction == null) : specialFunction.equals(rhs.specialFunction);
+		boolean att2 = (attributeName == null)? (rhs.attributeName == null) : attributeName.equals(rhs.attributeName);
+		return att1 && att2;
 	}
 	
 	@Override
@@ -54,6 +75,7 @@ public class AggregationValues implements Comparable<AggregationValues> {
 		}
 	}
 
+	@Override
 	public int compareTo(AggregationValues o) {
 		if (specialFunction != null && o.specialFunction != null) {
 			return specialFunction.compareTo(o.specialFunction);
