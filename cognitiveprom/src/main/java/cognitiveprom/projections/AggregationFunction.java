8bb391e3c4dd3c7c0ab63a5064f5fc4cf556c9b7
Andrea Burattin <andrea.burattin@gmail.com>
2018-05-30 08:21:00 +0200

    Removed adaptation of number

--- cognitiveprom/src/main/java/cognitiveprom/projections/AggregationFunction.java
+++ cognitiveprom/src/main/java/cognitiveprom/projections/AggregationFunction.java
@@ -11,46 +11,32 @@ public abstract class AggregationFunction implements Cloneable {
 			return new AggregationFunction("Sum", "#.###") {
 				@Override
 				public Number getValue() {
-					return adapt(stat.getSum());
+					return stat.getSum();
 				}
 			};
 		} else if (function == AggregationFunctions.MEAN) {
 			return new AggregationFunction("Mean", "#.###") {
 				@Override
 				public Number getValue() {
-					return adapt(stat.getMean());
+					return stat.getMean();
 				}
 			};
 		} else if (function == AggregationFunctions.MIN) {
 			return new AggregationFunction("Min", "#.###") {
 				@Override
 				public Number getValue() {
-					return adapt(stat.getMin());
+					return stat.getMin();
 				}
 			};
 		} else if (function == AggregationFunctions.MAX) {
 			return new AggregationFunction("Max", "#.###") {
 				@Override
 				public Number getValue() {
-					return adapt(stat.getMax());
-				}
-			};
-		} else if (function == AggregationFunctions.MEDIAN) {
-			return new AggregationFunction("Median", "#.###") {
-				@Override
-				public Number getValue() {
-					return adapt(stat.getPercentile(50));
+					return stat.getMax();
 				}
 			};
 		}
 		return null;
-	}
-	
-	private static Number adapt(Number number) {
-//		if ((long) number.doubleValue() == number.doubleValue()) {
-//			return number.longValue();
-//		}
-		return number;
 	}
 	
 	private String name;
