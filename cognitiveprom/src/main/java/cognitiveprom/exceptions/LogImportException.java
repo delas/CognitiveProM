87690da41d4d2ef84de01d0c5e95f4eb7e715455
Andrea Burattin <andrea.burattin@gmail.com>
2018-06-01 14:06:36 +0200

    Working on XES extension for cognitive data

---
+++ cognitiveprom/src/main/java/cognitiveprom/exceptions/LogImportException.java
@@ -0,0 +1,14 @@
+package cognitiveprom.exceptions;
+
+/**
+ * 
+ * @author Andrea Burattin
+ */
+public class LogImportException extends Exception {
+
+	private static final long serialVersionUID = 6611592367102399986L;
+
+	public LogImportException(String message) {
+		super(message);
+	}
+}
