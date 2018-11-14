d2fb5c56dfac400463d8ba675121fade2a8d36ee
Andrea Burattin <andrea.burattin@gmail.com>
2018-11-14 13:30:23 +0100

    Adapted importer to use XES-Lite

--- cognitiveprom/src/main/java/cognitiveprom/log/io/XesGzCognitiveImporter.java
+++ cognitiveprom/src/main/java/cognitiveprom/log/io/XesGzCognitiveImporter.java
@@ -2,12 +2,13 @@ package cognitiveprom.log.io;
 
 import java.io.File;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 import org.deckfour.xes.in.XParser;
-import org.deckfour.xes.in.XesXmlGZIPParser;
 import org.deckfour.xes.model.XLog;
 import org.processmining.framework.util.progress.XMonitoredInputStream;
 import org.processmining.framework.util.progress.XProgressListener;
+import org.xeslite.parser.XesLiteXmlParser;
 
 import cognitiveprom.annotations.Importer;
 import cognitiveprom.exceptions.LogIOException;
@@ -27,8 +28,8 @@ public class XesGzCognitiveImporter extends CognitiveLogImporter {
 	@Override
 	public CognitiveLog load(File fileName) throws LogIOException {
 		try {
-			XParser parser = new XesXmlGZIPParser();
-			List<XLog> logs = parser.parse(new XMonitoredInputStream(fileName, new XProgressListener() {
+			XParser parser = new XesLiteXmlParser(true);
+			List<XLog> logs = parser.parse(new GZIPInputStream(new XMonitoredInputStream(fileName, new XProgressListener() {
 				@Override
 				public void updateProgress(int progress, int maxProgress) {
 					triggerProgressUpdate(0, progress, maxProgress);
@@ -37,8 +38,10 @@ public class XesGzCognitiveImporter extends CognitiveLogImporter {
 				public boolean isAborted() {
 					return false;
 				}
-			}));
-			return new CognitiveLog(logs.get(0));
+			})));
+			CognitiveLog log = new CognitiveLog(logs.get(0));
+			
+			return log;
 		} catch (Exception e) {
 			throw new LogIOException(e.getMessage());
 		}
